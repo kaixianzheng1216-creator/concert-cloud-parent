cc656119bc9baeb45f29039fab8398c4e1ace2e5
kaixianzheng <kaixianzheng1216@gmail.com>
2026-01-27 21:58:18 +0800

    feat: 项目起步

---
+++ src/main/java/com/example/concertcloudparent/ConcertCloudParentApplication.java
@@ -0,0 +1,11 @@
+package com.example.concertcloudparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertCloudParentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConcertCloudParentApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/concertcloudparent/ConcertCloudParentApplicationTests.java
@@ -0,0 +1,10 @@
+package com.example.concertcloudparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertCloudParentApplicationTests {
+    @Test
+    void contextLoads() {}
+}
