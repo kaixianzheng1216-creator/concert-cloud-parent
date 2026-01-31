a24d5c56763fa629ec7db24dfa04ae23e443b6e5
kaixianzheng <kaixianzheng1216@gmail.com>
2026-01-31 18:44:09 +0800

    feat: 为 TracingConfig 添加@ Primary 注解

--- concert-common/src/main/java/com/example/concert/common/config/TracingConfig.java
+++ concert-common/src/main/java/com/example/concert/common/config/TracingConfig.java
@@ -3,10 +3,12 @@ package com.example.concert.common.config;
 import brave.sampler.Sampler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class TracingConfig {
     @Bean
+    @Primary
     public Sampler defaultSampler() {
         return Sampler.ALWAYS_SAMPLE;
     }
